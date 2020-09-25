accf8da3a18c97f837ad35cc44edbb0549938c0d
Patrick <50173111+PatrickDhamiKEA@users.noreply.github.com>
2020-09-25 22:52:51 +0200

    Add files via upload

---
+++ angry_birds_game.py
@@ -0,0 +1,157 @@
+class Bird:
+    def __init__(self, current_pos=[2, 2], bird_marker='B', start_pos=(2, 2),
+                 dir_left=False, dir_right=True, dir_up=False, dir_down=False):
+        self.bird_marker = bird_marker
+        self.start_pos = start_pos
+        self.current_pos = current_pos
+        self.dir_left = dir_left
+        self.dir_right = dir_right
+        self.dir_up = dir_up
+        self.dir_down = dir_down
+
+    def turn_left(self):
+        if self.dir_left:
+            self.dir_left = False
+            self.dir_up = False
+            self.dir_right = False
+            self.dir_down = True
+
+        elif self.dir_down:
+            self.dir_left = False
+            self.dir_up = False
+            self.dir_down = False
+            self.dir_right = True
+
+        elif self.dir_right:
+            self.dir_left = False
+            self.dir_right = False
+            self.dir_down = False
+            self.dir_up = True
+
+        elif self.dir_up:
+            self.dir_up = False
+            self.dir_right = False
+            self.dir_down = False
+            self.dir_left = True
+
+    def turn_right(self):
+        if self.dir_left:
+            self.dir_left = False
+            self.dir_right = False
+            self.dir_down = False
+            self.dir_up = True
+
+        elif self.dir_down:
+            self.dir_up = False
+            self.dir_down = False
+            self.dir_right = False
+            self.dir_left = True
+
+        elif self.dir_right:
+            self.dir_left = False
+            self.dir_right = False
+            self.dir_up = False
+            self.dir_down = True
+
+        elif self.dir_up:
+            self.dir_up = False
+            self.dir_down = False
+            self.dir_left = False
+            self.dir_right = True
+
+    def move_forward(self):
+        if self.dir_right:
+            self.current_pos[1] = self.current_pos[1] + 1
+        elif self.dir_left:
+            self.current_pos[1] = self.current_pos[1] - 1
+        elif self.dir_up:
+            self.current_pos[0] = self.current_pos[0] - 1
+        elif self.dir_down:
+            self.current_pos[0] = self.current_pos[0] + 1
+
+
+class Pig:
+    def __init__(self, pig_marker='P', pig_pos=(6, 6)):
+        self.pig_marker = pig_marker
+        self.pig_pos = pig_pos
+
+
+class Board(Bird, Pig):
+    def __init__(self, width=10, height=10):
+        self.board = [['*' for i in range(width)] for i in range(height)]
+        Bird.__init__(self)
+        Pig.__init__(self)
+
+    def set_bird_start_pos(self):
+        self.board[self.start_pos[0]][self.start_pos[1]] = self.bird_marker
+
+    def set_pig_pos(self):
+        self.board[self.pig_pos[0]][self.pig_pos[1]] = self.pig_marker
+
+    def display_board(self):
+        self.set_bird_start_pos()
+        self.set_pig_pos()
+        for row in self.board:
+            print('  '.join(row))
+        print('\n')
+
+
+class Workspace(Bird, Pig):
+    def __init__(self):
+        self.instructions = 'Bird starts facing right.\nMove forward press: f\nTurn left press: l\nTurn right press: r'
+        self.moves = []
+        Bird.__init__(self)
+        Pig.__init__(self)
+
+    def display_instructions(self):
+        print(self.instructions)
+
+    def get_user_moves(self):
+        while True:
+            user_input = input('Move: ')
+            if user_input == 'f' or user_input == 'r' or user_input == 'l':
+                self.moves.append(user_input)
+            elif user_input == 'q':
+                break
+            else:
+                print('Invalid move')
+
+        Workspace.move_bird(self)
+
+    def move_bird(self):
+        for i in self.moves:
+            if i == 'f':
+                self.move_forward()
+            elif i == 'r':
+                self.turn_right()
+            elif i == 'l':
+                self.turn_left()
+
+        Workspace.check_result(self)
+
+    def check_result(self):
+        if self.current_pos[0] < 0 or self.current_pos[0] > 9 or self.current_pos[1] < 0 or self.current_pos[1] > 9:
+            print('Birds final position: ', tuple(self.current_pos))
+            print('Pigs final position: ', self.pig_pos)
+            print('Bird lost')
+            print('Pig won')
+        elif tuple(self.current_pos) != self.pig_pos:
+            print('Birds final position: ', tuple(self.current_pos))
+            print('Pigs final position: ', self.pig_pos)
+            print('Bird lost')
+            print('Pig won')
+        else:
+            print('Birds final position: ', tuple(self.current_pos))
+            print('Pigs final position: ', self.pig_pos)
+            print('Bird won')
+            print('Pig lost')
+
+
+class Game:
+    def __init__(self):
+        Board().display_board()
+        Workspace().display_instructions()
+        Workspace().get_user_moves()
+
+
+Game()
